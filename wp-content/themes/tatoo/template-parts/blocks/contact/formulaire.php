463b39d712941876d2d3a282435bde8e55a8ce3f
Fabien Lapert <lapertfabien@gmail.com>
2023-11-14 14:16:34 +0100

    create custom contact

--- wp-content/themes/tatoo/template-parts/blocks/contact/formulaire.php
+++ wp-content/themes/tatoo/template-parts/blocks/contact/formulaire.php
@@ -3,22 +3,22 @@
     $phone = get_field('contact-phone', 'options');
     $email = get_field('contact-email', 'options');
     $iframe = get_field('location');
-    $form = get_field('formulaire');
     $title = get_field('form-title');
     $description = get_field('form-description');
 
-    if (!$form) {
-        return;
-    }
 ?>
 
 <div class="formulaire">
-    <div class="formulaire__form">
-        <?= do_shortcode($form) ?>
+    <form action="" class="formulaire__form">
+        <input type="text" name="nom" id="nom" placeholder="Nom / Prenom*" class="formulaire__form__input">
+        <input type="text" name="email" id="email" placeholder="Email*" class="formulaire__form__input">
+        <input type="text" name="subject" id="subject" placeholder="Objet*" class="formulaire__form__input">
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message*" class="formulaire__form__input"></textarea>
         <span class="formulaire__form__required">
             Les champs munis d&rsquo;un astérisque (*) sont obligatoires.
         </span>
-    </div>
+        <input type="submit" value="Envoyer" class="formulaire__form__submit">
+    </form>
     <div class="formulaire__coordonnees">
         <div class="formulaire__coordonnees__infos">
             <h2 class="formulaire__coordonnees__items__infos__title">
